628a4effc6513a5171d52408eff02e25384e1707
mrxuxg <mrxuxg@gmail.com>
2021-05-27 11:36:59 +0800

    Update JZ3

---
+++ PointToOffer/JZ3_PrintListInReversedOrder.cpp
@@ -0,0 +1,22 @@
+/*
+    思路：
+        打印节点值是一个读操作，我们尽量不应该修改原链表的结构，所以不建议反转链表之后再输出。
+        从头遍历，从尾输出，这是典型的“先进后出”，应该采用栈。
+*/
+std::vector<int> printListInReversedOrder(ListNode* head){
+    ListNode* curNode = head;
+    std::stack<ListNode*> stk;
+    std::vector<int> res;
+    while (curNode != nullptr){
+        stk.push(curNode);
+        curNode = curNode->next;
+    }
+    while (!stk.empty())
+    {
+        ListNode* n = stk.top();
+        stk.pop();
+        res.push_back(n->val);
+    }
+    
+    return res;
+}
