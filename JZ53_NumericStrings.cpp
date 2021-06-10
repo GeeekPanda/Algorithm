7bce4f2b763f7eac2448599950274abd395d9375
mrxuxg <mrxuxg@gmail.com>
2021-06-10 21:21:37 +0800

    fix

--- JZ53_NumericStrings.cpp
+++ JZ53_NumericStrings.cpp
@@ -4,23 +4,71 @@
         例如，字符串"+100","5e2","-123","3.1416"和"-1E-16"都表示数值。 
         但是"12e","1a3.14","1.2.3","+-5"和"12e+4.3"都不是。
     思路：
-        本题使用有限状态自动机。根据字符类型和合法数值的特点，先定义状态，
-        再画出状态转移图，最后编写代码即可。
-        字符类型：
-            - 空格「 」
-            - 数字「0-9」
-            - 正负号「+—」
-            - 小数点「.」
-            - 幂符号「eE」
-        状态定义：
-            按照字符串从左到右的顺序，定义以下 9 种状态：
-            开始的空格
-            幂符号前的正负号
-            小数点前的数字
-            小数点、小数点后的数字
-            当小数点前为空格时，小数点、小数点后的数字
-            幂符号
-            幂符号后的正负号
-            幂符号后的数字
-            结尾的空格
-*/s
+        A[.[B]][e|EC]或者.B[e|EC]是两种正确的模式
+        A C可能以+ -开头。
+*/
+class Solution {
+private:
+    // 整数的格式可以用[+|-]B表示, 其中B为无符号整数
+    bool scanInteger(const string s, int& index){
+
+        if(s[index] == '+' || s[index] == '-')
+            ++index;
+
+        return scanUnsignedInteger(s, index);
+    }
+    
+    bool scanUnsignedInteger(const string s, int& index){
+
+        int befor = index;
+        while(index != s.size() && s[index] >= '0' && s[index] <= '9')
+            index ++;
+
+        return index > befor;
+    }
+public:
+    // 数字的格式可以用A[.[B]][e|EC]或者.B[e|EC]表示，
+    // 其中A和C都是整数（可以有正负号，也可以没有），而B是一个无符号整数
+    bool isNumber(string s) {
+
+        if(s.size() == 0)
+            return false;
+        int index = 0;
+
+        //字符串开始有空格，可以返回true
+        while(s[index] == ' ')  //书中代码没有该项测试
+            ++index;
+
+        bool numeric = scanInteger(s, index);
+
+        // 如果出现'.'，接下来是数字的小数部分
+        if(s[index] == '.'){
+
+            ++index;
+
+            // 下面一行代码用||的原因：
+            // 1. 小数可以没有整数部分，例如.123等于0.123；
+            // 2. 小数点后面可以没有数字，例如233.等于233.0；
+            // 3. 当然小数点前面和后面可以有数字，例如233.666
+            numeric = scanUnsignedInteger(s, index) || numeric;
+        }
+
+        // 如果出现'e'或者'E'，接下来跟着的是数字的指数部分
+        if(s[index] == 'e' || s[index] == 'E'){
+
+            ++index;
+
+            // 下面一行代码用&&的原因：
+            // 1. 当e或E前面没有数字时，整个字符串不能表示数字，例如.e1、e1；
+            // 2. 当e或E后面没有整数时，整个字符串不能表示数字，例如12e、12e+5.4
+            numeric = numeric && scanInteger(s ,index);
+        }
+
+        //字符串结尾有空格，可以返回true
+        while(s[index] == ' ')
+            ++index;
+        cout << s.size() << " " << index;   //调试用
+
+        return numeric && index == s.size();
+    }
+};
