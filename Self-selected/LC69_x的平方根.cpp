9de74ccf642c1f0201f2837b69613ec5addc66f3
mrxuxg <mrxuxg@gmail.com>
2021-07-08 12:43:20 +0800

    update sqrt

---
+++ Self-selected/LC69_x的平方根.cpp
@@ -0,0 +1,30 @@
+/*
+    题目：
+        实现 int sqrt(int x) 函数。
+        计算并返回 x 的平方根，其中 x 是非负整数。
+        由于返回类型是整数，结果只保留整数的部分，小数部分将被舍去。
+    思路：
+        牛顿迭代法
+*/
+
+#include <iostream>
+#include <cmath>
+
+class Solution {
+public:
+    double mySqrt(int x) {
+        if (x == 0) {
+            return 0;
+        }
+
+        double C = x, x0 = x;
+        while (true) {
+            double xi = 0.5 * (x0 + C / x0);
+            if (fabs(x0 - xi) < 1e-7) {
+                break;
+            }
+            x0 = xi;
+        }
+        return x0;
+    }
+};
